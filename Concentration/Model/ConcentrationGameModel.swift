d0d677a16982cb74e24919c3a14870e541310c13
dianaoros <dianaorosb@gmail.com>
2020-07-23 15:56:34 -0700

    Initial Functionality and UI
    Add UI buttons and labels and set Autolayout
    Add Functionality
    Adopt to MVC

--- Concentration/Model/ConcentrationGameModel.swift
+++ Concentration/Model/ConcentrationGameModel.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+class ConcentrationGameModel {
+    
+    var cards = [CardModel]()
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        
+        if let matchedIndex = indexOfOneAndOnlyFaceUpCard, matchedIndex != index {
+            if cards[matchedIndex].identifier == cards[index].identifier {
+                cards[matchedIndex].isMatched = true
+                cards[index].isMatched = true
+            }
+            cards[index].isFaceUp = true
+            indexOfOneAndOnlyFaceUpCard = nil
+            
+        } else {
+            for flipDownIndex in cards.indices {
+                cards[flipDownIndex].isFaceUp = false
+            }
+            cards[index].isFaceUp = true
+            indexOfOneAndOnlyFaceUpCard = index
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 0..<numberOfPairsOfCards {
+            let card = CardModel()
+            cards += [card, card]
+            cards.shuffle()
+        }
+    }
+
+    //TODO: Reset game
+
+}
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,6 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var game = ConcentrationGameModel(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    
+    var flipsCount = 0 {
+        didSet {
+            flipsLabel.text = ("Flips: \(flipsCount)")
+        }
+    }
+   
+    var emojiArray = ["🎃", "👻", "💀", "🍎", "🍭", "🍬", "🦇", "😈", "🙀", "🤡"]
+    var emojiDictionary = [Int: String]()
+    
+    @IBOutlet weak var flipsLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipsCount += 1
+        
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        }
+        else {
+            print("Chosen Card was not in cardButtons array")
+        }
+        
+    }
+    
+    func updateViewFromModel() {
+        
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                button.setTitle(getEmoji(for: card), for: .normal)
+            }
+            else {
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+                button.setTitle("", for: .normal)
+            }
+        }
+    }
+    
+    
+    func getEmoji(for card: CardModel) -> String {
+        if emojiDictionary[card.identifier] == nil, emojiArray.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiArray.count)))
+            emojiDictionary[card.identifier] = emojiArray.remove(at: randomIndex)
+        }
+        return emojiDictionary[card.identifier] ?? "?"
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
